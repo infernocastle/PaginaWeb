8fbe1eca9f7ee495afe8ea24f0493ba14ad8101b
infernocastle <74935620+infernocastle@users.noreply.github.com>
2023-07-24 21:36:32 -0500

    Add files via upload

---
+++ js/Main.js
@@ -0,0 +1,12 @@
+function suma() {
+
+    let A = 0;
+    let B = 0;
+    let SUMA = 0
+    A = parseInt(prompt("Por favor ingrese el primer valor"))
+    B = parseInt(prompt("Por favor ingrese el segundo valor"))
+
+    SUMA = A + B;
+
+    alert("La suma es igual a: " + SUMA);
+}
